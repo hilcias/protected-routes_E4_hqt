3d9746e00b738cfae4a99b508971ebb6038bb0e6
hilcias <112222699+hilcias@users.noreply.github.com>
2022-12-21 08:49:01 -0500

    Add files via upload

---
+++ src/auth/auth.services.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+const checkUserCredentials = require('./auth.controller')
+const jwtSecret = require('../../config').api.jwtSecret
+
+const postLogin = (req, res) => {
+    const { email, password } = req.body
+
+    if(email && password) {
+        checkUserCredentials(email, password)
+            .then((data) => {
+                if(data) {
+                    const token = jwt.sign({
+                        id: data.id,
+                        email: data.email,
+                        role: data.role
+                    }, jwtSecret)
+                    
+                    res.status(200).json({
+                        message: 'Correct Credentials', token
+                    })
+                } else {
+                    res.status(401).json({message: 'Invalid Credentials'})
+                }
+            })
+            .catch((err) => {
+                res.status(400).json({message: err.message})
+            })
+    } else {
+        res.status(400).json({message: 'Missing data', fields: {email: 'example@example.com', password: 'String'}})
+    }
+}
+
+module.exports = {
+    postLogin
+}
